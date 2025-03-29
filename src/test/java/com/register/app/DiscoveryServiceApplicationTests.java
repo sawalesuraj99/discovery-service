6115a69f0029604b5ae1cbb66fb76dc9a22c1d1f
Suraj sawale <sawalesuraj99@gmail.com>
2025-03-29 19:53:25 +0530

    add discovery service

---
+++ src/test/java/com/register/app/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.register.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
